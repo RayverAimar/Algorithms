2f8bd538e3b39f6e2dd0ce47a6cd3eab56084cbd
Rayver Aimar <rayver.munoz@ucsp.edu.pe>
2022-02-08 15:46:33 -0500

    New version Now it shows full path from end to final node

--- Graph/Dijkstra/dijkstra.cpp
+++ Graph/Dijkstra/dijkstra.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <stack>
 
 using namespace std;
 
@@ -15,10 +16,11 @@ void InsEdge(vector<vector<pair<int,int>>> &nodes, int start, int end, int weigh
 
 void dijkstra(vector<vector<pair<int,int>>> &nodes, int start, int end){
     int N = nodes.size();
-    vector<int> dist(N, INF);
+    vector<pair<int, int>> dist(N, {INF, -1});
+    vector<int> shortest_path;
     vector<bool> visited(N, 0);
     priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int, int>>> pq;
-    dist[start] = 0;
+    dist[start].first = 0;
     pq.push(make_pair(0,start));
     while(!pq.empty()){
         int cur_node = pq.top().second;
@@ -26,13 +28,28 @@ void dijkstra(vector<vector<pair<int,int>>> &nodes, int start, int end){
         if(visited[cur_node]) continue;
         visited[cur_node] = 1;
         for(auto edge : nodes[cur_node]){
-            if(dist[cur_node] + edge.second < dist[edge.first]){
-                dist[edge.first] = dist[cur_node] + edge.second;
-                pq.push(make_pair(dist[edge.first], edge.first));
+            if(dist[cur_node].first + edge.second < dist[edge.first].first){
+                dist[edge.first].first = dist[cur_node].first + edge.second;
+                dist[edge.first].second = cur_node;
+                pq.push(make_pair(dist[edge.first].first, edge.first));
             }
         }
     }
-    cout << "Shortest distance from "<< ++start <<" to "<< end + 1 <<" is "<<dist[end]<<"\n";
+    stack<int> s;
+    int pos = end;
+    do{
+        s.push(pos);
+        pos = dist[pos].second; 
+    }while(pos != -1);
+    while(!s.empty()){
+        shortest_path.push_back(s.top() + 1);
+        s.pop();
+    }
+    cout << "Shortest distance from "<< ++start <<" to "<< end + 1 <<" is "<<dist[end].first<<"\n";
+    cout <<"Path is: ";
+    for(int i = 0; i < shortest_path.size(); i++)
+        cout<<shortest_path[i]<<" ";
+
 } //dijkstra
 
 } //namespace graph
