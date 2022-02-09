ce432719a9b46b0403198fc0c292a7bd2dab4ddb
Rayver Aimar <rayver.munoz@ucsp.edu.pe>
2022-02-09 12:43:51 -0500

    Modifying kruskal algorithm Now it shows all taken edges and their weight

--- Graph/Kruskal/kruskal.cpp
+++ Graph/Kruskal/kruskal.cpp
@@ -18,16 +18,22 @@ int root(int i, int* parent){ //Search for the parent whose tag is its position
 void kruskal(vector<pair<int , pair<int, int>>>& edges){
     int min_weight{0}, *parent = new int[n_nodes];
     size_t N = edges.size();
+    vector<pair<pair<int, int>, int>> kruskal_path;
     for(int i = 0; i < n_nodes; i++) parent[i] = i;
     for(int i = 0; i < N; i++){
         int start = edges[i].second.first, end = edges[i].second.second,
             root_start = root(start, parent), root_end = root(end, parent);
         if(root_start != root_end){
+            kruskal_path.push_back({{start, end}, edges[i].first});
             min_weight += edges[i].first;
             parent[root_start] = parent[root_end];
         }
     }
-    cout << "MST weight made by Kruskal Algorithm is: " << min_weight;
+    cout<<"Printing MST's edges: \n";
+    for(int i = 0; i < n_nodes - 1; i++)
+        cout <<"\t"<<kruskal_path[i].first.first + 1 <<"->"<<kruskal_path[i].first.second + 1
+            <<" ("<<kruskal_path[i].second<<")\n";      
+    cout << "MST's weight made by Kruskal Algorithm is: " << min_weight;
 }
 
 int main(){
