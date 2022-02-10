4ffe523c8a02c94c4916c80b128c55f65b46ddf6
Rayver Aimar <rayver.munoz@ucsp.edu.pe>
2022-02-09 21:02:32 -0500

    Modifying topological sort Now every node has a key (string)

--- Graph/Topological-Sorting/topological_sort.cpp
+++ Graph/Topological-Sorting/topological_sort.cpp
@@ -1,16 +1,21 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <map>
+#include <string>
 
 using namespace std;
 
+string name;
 size_t n_nodes{0}, n_edges{0};
+map<int, string> node_names;
 vector<vector<int>> graph;
 vector<bool> visited;
 vector<int> tp_order;
 
 void dfs(int u){
     visited[u] = true;
+    
     for(int v : graph[u]){
         if(!visited[v]) dfs(v);
     }
@@ -22,14 +27,18 @@ void topological_sort(){
         if(!visited[i]) dfs(i);
     }
     reverse(tp_order.begin(), tp_order.end());
-    for(int n : tp_order)
-        cout << n + 1 << " ";
+    for(int i = 0; i < n_nodes; i++)
+        cout << node_names[tp_order[i]] << " ";
 
 }
 int main(){
     cin>>n_nodes>>n_edges;
     graph.resize(n_nodes, vector<int>());
     visited = vector<bool> (n_nodes, 0);
+    for(int i = 0; i < n_nodes; i++){
+        cin>>name;
+        node_names.insert({i, name});
+    }
     while(n_edges--){
         size_t start, end;
         cin>>start>>end;
