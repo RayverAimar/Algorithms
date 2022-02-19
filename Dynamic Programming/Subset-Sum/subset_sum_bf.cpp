2a506022043330f9fc56fa1acfd6b8e045f4ed30
Rayver Aimar <rayver.munoz@ucsp.edu.pe>
2022-02-18 23:01:09 -0500

    Cleaning code

--- Dynamic Programming/Subset-Sum/subset_sum_bf.cpp
+++ Dynamic Programming/Subset-Sum/subset_sum_bf.cpp
@@ -4,32 +4,35 @@
 
 using namespace std;
 
+#define tt ll testcase; cin>>testcase; while(testcase--)
 #define FORN(i,m,n) for(ll i=(m); i<int(n); i++)
+#define p_b(x) push_back(x)
 typedef long long ll;
 
-bool subset_sum(vector<ll> n, ll target) {
+int n;
+
+bool subset_sum(vector<ll> arr, ll sum) {
     ll max_sum{ 0 }, min_sum{ 0 };
-    for (ll a : n) a < 0 ? min_sum += a : max_sum += a;
-    if (target < min_sum || target > max_sum) return 0;
-    FORN(bm, 1, 16) {
-        ll ans{ target };
-        FORN(i, 0, 4) if (bm & 1 << i) ans -= n[i];
-        if (ans == 0) return 1;
+    for (ll a : arr) a < 0 ? min_sum += a : max_sum += a;
+    if (sum < min_sum || sum > max_sum) return 0;
+    FORN(bm, 1, 1<<n) {
+        ll ans{ sum };
+        FORN(i, 0, 4) if (bm & 1 << i) ans -= arr[i];
+        if (!ans) return 1;
     }
     return 0;
 }
 
 int main() {
-    ll t;
-    cin >> t;
-    while (t--) {
-        vector<ll> n; ll target;
-        FORN(i, 0, 4) {
+    tt{
+        vector<ll> arr; ll sum;
+        cin>>n;
+        FORN(i, 0, n) {
             ll a;
             cin >> a;
-            n.push_back(a);
+            arr.p_b(a);
         }
-        cin >> target;
-        cout << (subset_sum(n, 0) ? "YES" : "NO") << "\n";
+        cin >> sum;
+        cout << (subset_sum(arr, sum) ? "YES" : "NO") << "\n";
     }
 }
--- Dynamic Programming/Subset-Sum/subset_sum_td.cpp
+++ Dynamic Programming/Subset-Sum/subset_sum_td.cpp
@@ -3,19 +3,19 @@
 
 using namespace std;
 
+#define tt ll testcase; cin>>testcase;while(testcase--)
 #define FORN(i,m,n) for(int i = (m); i < int(n); i++)
 typedef long long ll;
 
-size_t t;
 int arr[100], stk[100], top=-1, n;
 
 bool subset_sum(int i, int sum){
     if(!sum){
         FORN(i,0,top+1) cout<<stk[i]<<" ";
         cout<<"\n";
-        return true;
+        return 1;
     }
-    if(i >= n) return false;
+    if(i >= n) return 0;
     stk[++top] = arr[i];
     bool on = subset_sum(i+1, sum-arr[i]);
     --top;
@@ -24,12 +24,11 @@ bool subset_sum(int i, int sum){
 }
 
 int main(){
-    cin>>t;
-    while(t--){
+    tt{
         cin>>n;
         FORN(i,0,n) cin>> arr[i];
         int sum;
         cin>>sum;
-        cout<<(subset_sum(0, sum) ? "YES" : "NO")<<endl;
+        cout<<(subset_sum(0, sum) ? "YES" : "NO")<<"\n";
     }
 }
