91cfdaffca0fd137e05f338965602596f6172d82
Rayver Aimar <rayver.munoz@ucsp.edu.pe>
2022-02-25 23:48:57 -0500

    Adding Tree Height function

---
+++ Dynamic Programming/Tree-Height/tree_height.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+
+#define tt int testcase; cin>>testcase; while(testcase--)
+#define FORN(i,m,n) for(int i = m; i < n; i++)
+using namespace std;
+
+vector<bool> visited;
+vector<int> dp;
+vector<vector<int>> adj;
+
+void dfs(int u){
+    visited[u] = true;
+    int cur_height=1;
+    for(int n : adj[u]){
+        if(!visited[n])
+        {
+            dfs(n);
+            cur_height=max(cur_height, dp[n]+1);
+        }
+    }
+    dp[u]=cur_height;
+}
+
+int main(){
+    tt{
+        size_t n_nodes;
+        cin>>n_nodes;
+        adj=vector<vector<int>>(n_nodes-1, vector<int>());
+        FORN(i,0,n_nodes-1){
+            size_t start, end;
+            cin>>start>>end;
+            adj[start].push_back(end);
+            adj[end].push_back(start);
+        }
+        visited = vector<bool>(n_nodes+1, 0);
+        dp=vector<int>(n_nodes+1, 0);
+        dfs(1);
+        cout<<dp[1];
+    }
+}
